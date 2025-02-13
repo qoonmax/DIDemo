e087a34559a23798dc15a95896a70a448b9c1d05
qoonmax <qoonmax@gmail.com>
2025-02-13 12:46:50 +0400

    Initial Commit

---
+++ DIDemo/DIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIDemoApp.swift
+//  DIDemo
+//
+//  Created by Максим Марков on 13.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct DIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DIDemoTests/DIDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  DIDemoTests.swift
+//  DIDemoTests
+//
+//  Created by Максим Марков on 13.02.2025.
+//
+
+import Testing
+@testable import DIDemo
+
+struct DIDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
